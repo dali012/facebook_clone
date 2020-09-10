bdfbaabf1676c029152a7973aaf0057ff2b99adf
dali012 <dali.jerbi97@gmail.com>
2020-09-10 17:17:20 +0100

    adding firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAFbHxPMmNxDa7zqQRHqlw_-ZbiE3N_DWI",
+  authDomain: "facebookclone-8e348.firebaseapp.com",
+  databaseURL: "https://facebookclone-8e348.firebaseio.com",
+  projectId: "facebookclone-8e348",
+  storageBucket: "facebookclone-8e348.appspot.com",
+  messagingSenderId: "300153042373",
+  appId: "1:300153042373:web:aceaf5d8a4e4e2f62dc7ac",
+  measurementId: "G-XHT6LS40CY"
+};
